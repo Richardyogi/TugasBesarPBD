8cd36250933d8ac8797eb3f57ab6af54b6e3fb32
Richardyogi <richardwilliam02@gmail.com>
2018-04-13 15:09:41 +0700

    masukin file php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>\
+<html>
+    <head>
+        <title> Lab Logging </title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
