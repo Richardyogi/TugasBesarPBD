96256ee204beeae3673816da3b30d97fea655032
Adriswara <chevellerz454@yahoo.com>
2018-04-25 14:07:13 +0700

    converted from ci to pure php page

--- jamSibuk.php
+++ jamSibuk.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,32 +21,32 @@
 
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="assets/main.css">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-sm bg-dark  navbar-dark">
-        <a class="navbar-brand font-weight-heavy" href="index.html" style="color: #F08519;font-family: Calibri">Labkom
-            <a class="navbar-brand" href="index.html" style="color: white;margin-left: -1.1%;margin-right: 1%;font-family: Calibri">Logs</a>
+        <a class="navbar-brand font-weight-heavy" href="index.php" style="color: #F08519;font-family: Calibri">Labkom
+            <a class="navbar-brand" href="index.php" style="color: white;margin-left: -1.1%;margin-right: 1%;font-family: Calibri">Logs</a>
         </a>
 
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link " href="input.html">Input data</a>
+                <a class="nav-link " href="input.php">Input data</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link " href="rRobin.html">Round Robin</a>
+                <a class="nav-link " href="rRobin.php">Round Robin</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
                     Laporan Lainnya
                 </a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="jamSibuk.html">Catatan Jam Sibuk</a>
-                    <a class="dropdown-item" href="pengKom.html">Jumlah Penggunaan Komputer</a>
-                    <a class="dropdown-item" href="waktuPeng.html">Waktu Penggunaan Komputer</a>
-                    <a class="dropdown-item" href="pengApp.html">Penggunaan Aplikasi</a>
-                    <a class="dropdown-item" href="lprnPeng.html">Laporan Pengguna</a>
+                    <a class="dropdown-item" href="jamSibuk.php">Catatan Jam Sibuk</a>
+                    <a class="dropdown-item" href="pengKom.php">Jumlah Penggunaan Komputer</a>
+                    <a class="dropdown-item" href="waktuPeng.php">Waktu Penggunaan Komputer</a>
+                    <a class="dropdown-item" href="pengApp.php">Penggunaan Aplikasi</a>
+                    <a class="dropdown-item" href="lprnPeng.php">Laporan Pengguna</a>
                 </div>
             </li>
         </ul>
