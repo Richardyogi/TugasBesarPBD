fd5fafcfd2764dced089e1c009715165de53b2f9
Adriswara <chevellerz454@yahoo.com>
2018-05-04 17:10:09 +0700

    updated navItem

---
+++ connection.1.php
@@ -0,0 +1,15 @@
+<?php
+    $serverName = "DESKTOP-HKGSIC4\SQLEXPRESS01"; //ubah nama server sesuai di komputer masing"
+
+    // Since UID and PWD are not specified in the $connectionInfo array,
+    // The connection will be attempted using Windows Authentication.
+    $connectionInfo = array( "Database"=>"master");
+    $conn = sqlsrv_connect( $serverName, $connectionInfo);
+    
+    if( $conn ) {
+         //echo "Connection established.<br />";
+    }else{
+         echo "Connection could not be established.<br />";
+         die( print_r( sqlsrv_errors(), true));
+    }
+?>
