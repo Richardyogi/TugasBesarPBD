731c8a91b78b99a0fe9a60a671a58e426db4471e
arlin shiffa <sasqia.shiffa@gmail.com>
2018-05-07 19:09:25 +0700

    ganti nama sp agregat aktivitas userSP dan membetulkan agr waktu

--- SQL/sp/agr_aktivitas_userSP.sql
+++ SQL/sp/agr_aktivitas_userSP.sql
@@ -1,4 +1,4 @@
-create procedure agr_aktivitas_user
+create procedure agr_aktivitas_userSP
 	@date date
 as
 insert into agr_aktivitas_user
--- SQL/sp/agr_waktu_penggunaan.sql
+++ SQL/sp/agr_waktu_penggunaan.sql
@@ -1,8 +1,7 @@
-alter procedure agr_waktu_penggunaan
+create procedure agr_waktu_penggunaan
+	@date date
 as
 
-declare @currentTime time
-set @currentTime = '08:00:00'
 declare @n int
 set @n =0
 declare @awal time
@@ -12,162 +11,47 @@ set @akhir = '09:00:00'
 
 declare @tempTable table
 (
-	idPenggunaTemp int,
-	idUserTemp int,
 	tanggalTemp date,
-	jamTemp time,
-	statusTemp int
-)
-
-declare @tempTable2 table
-(
-	tanggalTemp2 date,
-	jamTemp2 time,
-	statusTemp2 int
+	jamAwalTemp time,
+	jamAkhirTemp time
 )
 
 insert into @tempTable
 select
-	idPengguna,
-	idUser,
 	tanggal, 
-	jam, 
-	status
-FROM testTBLRoundRobin(convert(date, CURRENT_TIMESTAMP)) 
-
---insert berurutan untuk tempTable
-declare myCursor1 cursor
-for
-	select 
-		tanggalTemp,
-		jamTemp,
-		statusTemp
-	from
-		@tempTable
-	order by idUserTemp,idPenggunaTemp
-
-open myCursor1
-
-declare @tanggal date
-declare @time time
-declare @status int
-
-fetch next from
-	myCursor1
-into
-	@tanggal,
-	@time,
-	@status
-
-while(@@FETCH_STATUS=0)
-begin 
-	insert into @tempTable2
-	select
-		@tanggal,
-		@time,
-		@status
-
-	fetch next from
-		myCursor1
-	into
-		@tanggal,
-		@time,
-		@status
-end
-
-close myCursor1 
-deallocate myCursor1
+	waktuMulai, 
+	waktuAkhir
+FROM dbo.rekapRoundRobin(@date) 
 
 declare @jumlahPengguna int
 set @jumlahPengguna=0
 
 while(@n < 11)
 begin
-	select @jumlahPengguna = count(*) 
+	select 
+		@jumlahPengguna = count(*) 
+	from 
+		@tempTable 
+	where
+		(jamAwalTemp<= @awal and jamAkhirTemp>@awal) or
+		(jamAwalTemp>= @awal and jamAwalTemp<@akhir) 
+
 	insert into agr_jumlah_pengguna_per_jam
 	select
-		CONVERT (DATE, CURRENT_TIMESTAMP),
-		@currentTime,
-		DATEADD(second,3599,@currentTime),
-		0		 
+		@date,
+		@awal,
+		DATEADD(second,3599,@awal),
+		@jumlahPengguna 
 
-	set @currentTime= DATEADD(hour,1,@currentTime)
+	set @awal= DATEADD(hour,1,@awal)
+	set @akhir= DATEADD(hour,1,@akhir)
 	set @n=@n+1
 end
 
+--declare @k date
+--set @k= convert(date, CURRENT_TIMESTAMP)
+----select * from dbo.rekapRoundRobin(@n)
+----exec agr_waktu_penggunaan @k
 
-
-
-------- variable start time dan end time ----
---declare @startTime time
---declare @endTime time
-
------------time dan status end when-------
---declare @time2 time
---declare @status2 int
-
---------cursor  start------------
---declare cursorStart cursor
---for
---	select
---		jamTemp2
---	from 
---		@tempTable2
---	where
---		statusTemp2=1
-
---------cursor End------------
---declare cursorEnd cursor
---for
---	select
---		jamTemp2
---	from 
---		@tempTable2
---	where
---		statusTemp2=0
-
---open cursorStart
---open cursorEnd
-
---fetch next from 
---	cursorStart
---into
---	@time
-
---fetch next from 
---	cursorEnd
---into
---	@time2
-	
---while(@@FETCH_STATUS=0)
---begin
-
---	while(@time<DATEADD(hour, 1, @time2))
---	begin
---		if(@time<DATEADD(hour,1,@time2))
---		begin
---			update agr_jumlah_pengguna_per_jam
---			set jumlah_pengguna = jumlah_pengguna+1
---			where agr_jumlah_pengguna_per_jam.start_time<=@time and agr_jumlah_pengguna_per_jam.end_time>=@time
-			
---			set @time = DATEADD(hour, 1, @time)
---		end		
---	end
---	fetch next from 
---		cursorStart
---	into
---		@time
-
---	fetch next from 
---		cursorEnd
---	into
---		@time2
---end
-
---close cursorStart
---deallocate cursorStart
-
---close cursorEnd
---deallocate cursorEnd
-
-
+--select * from agr_jumlah_pengguna_per_jam
+--select * from dbo.rekapRoundRobin(@k)
